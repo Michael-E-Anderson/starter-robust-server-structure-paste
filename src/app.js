81ce73230f0962c7a9cdd6d339cc3a2f2dc35d7f
Michael Anderson <manderson3992@yahoo.com>
2022-05-26 09:49:52 -0400

    Organizing Express Code

--- src/app.js
+++ src/app.js
@@ -5,16 +5,7 @@ const pastesRouter = require("./pastes/pastes.router")
 
 app.use(express.json());
 
-app.use("/pastes/:pasteId", (req, res, next) => {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
 
-  if (foundPaste) {
-    res.json({ data: foundPaste });
-  } else {
-    next({ status: 404, message: `Paste id not found: ${pasteId}` });
-  }
-});
 
 app.use("/pastes", pastesRouter);
 
--- src/pastes/pastes.controller.js
+++ src/pastes/pastes.controller.js
@@ -45,16 +45,95 @@ function exposurePropertyIsValid(req, res, next) {
     })
 }
 
+function syntaxPropertyIsValid(req, res, next) {
+    const { data: { syntax } = {} } = req.body;
+    const validSyntax = ["None", "Javascript", "Python", "Ruby", "Perl", "C", "Scheme"];
+    if (validSyntax.includes(syntax)) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `Value of the 'syntax' property must be one of ${validSyntax}. Received: ${syntax}.`,
+    })
+}
+
+function expirationIsValidNumber(req, res, next) {
+    const { data: { expiration } = {} } = req.body;
+    if (expiration <= 0 || !Number.isInteger(expiration)) {
+        return next({
+            status: 400,
+            message: `Expiration requires a valid number.`
+        })
+    }
+    next();
+}
+
+function pasteExists(req, res, next) {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    return next()
+  } else {
+    next({ status: 404, message: `Paste id not found: ${pasteId}` });
+  }
+};
+
+function read(req, res) {
+    const { pasteId } = req.params;
+    const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+    res.json({ data: foundPaste })
+}
+
+function update(req, res) {
+    const { pasteId } = req.params;
+    const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+    const { data: { name, syntax, expiration, exposure, text } = {} } = req.body;
+
+    foundPaste.name = name;
+    foundPaste.syntax  = syntax;
+    foundPaste.expiration = expiration;
+    foundPaste.exposure = exposure;
+    foundPaste.text = text;
+
+    res.json({ data: foundPaste })
+}
+
+function destroy(req, res) {
+    const { pasteId } = req.params;
+    const index = pastes.findIndex((paste) => paste.id === Number(pasteId))
+    const deletedPastes = pastes.splice(index, 1)
+    res.sendStatus(204)
+}
 
 
 module.exports = {
-    create: [
-        bodyDataHas("name"),
-        bodyDataHas("syntax"),
-        bodyDataHas("exposure"),
-        bodyDataHas("expiration"),
-        bodyDataHas("text"),
-        bodyDataHas("user_id"),
-        create],
-    list,
-}
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("syntax"),
+    bodyDataHas("exposure"),
+    bodyDataHas("expiration"),
+    bodyDataHas("text"),
+    bodyDataHas("user_id"),
+    exposurePropertyIsValid,
+    syntaxPropertyIsValid,
+    expirationIsValidNumber,
+    create,
+  ],
+  list,
+  read: [pasteExists, read],
+  update: [
+    pasteExists,
+    bodyDataHas("name"),
+    bodyDataHas("syntax"),
+    bodyDataHas("exposure"),
+    bodyDataHas("expiration"),
+    bodyDataHas("text"),
+    bodyDataHas("user_id"),
+    exposurePropertyIsValid,
+    syntaxPropertyIsValid,
+    expirationIsValidNumber,
+    update
+  ],
+  delete: [pasteExists, destroy],
+};
